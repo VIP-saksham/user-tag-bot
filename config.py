ed0900461f5b10a85316a32f66477430a3621488
VIP-saksham <93027065+VIP-saksham@users.noreply.github.com>
2026-01-08 03:42:00 +0530

    Add userbot configuration settings

---
+++ config.py
@@ -0,0 +1,19 @@
+# ===== USERBOT CONFIG =====
+
+API_ID = 21111        # <-- apna API_ID
+API_HASH = "0b52aa37a124e6867ee42bd9dcfbb98f"  # <-- apna API_HASH
+SESSION = "tag_userbot"  # session name
+
+# SUDO USER (sirf ye command use karega)
+SUDO_USERS = [7901717397,8321375544]  # apna telegram user id
+
+# TARGET GROUP (sirf ek)
+TARGET_GROUP = -1002517438543  # target gc id
+
+# LOGGER GROUP / PRIVATE CHAT ID
+LOG_CHAT = -1003467384062  # jahan log jayega
+
+#WORDS LIST (shuffle hoga)
+TAG_WORDS = [
+   ""
+]
